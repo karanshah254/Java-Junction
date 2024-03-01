af61b1c9fb87e7462d1e03ac77c7c05d6a68a7d0
karanshah254 <karankshah2504@gmail.com>
2024-03-01 13:39:29 +0530

    Optimzed java code improved performance

---
+++ OOP-VGEC/Practical-2/program_5.java
@@ -0,0 +1,25 @@
+public class program_5 {
+    public static void main(String[] args) {
+        int number[] = { 2, 6, 5, 1, 8, 9, -1, -4, 10, 15 };
+        System.out.print("Array elements are : ");
+        for (int i = 0; i < number.length; i++) {
+            System.out.print(number[i] + " ");
+        }
+
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i + 1; j < number.length; j++) {
+                int temp = 0;
+                if (number[i] > number[j]) {
+                    temp = number[i];
+                    number[i] = number[j];
+                    number[j] = temp;
+                }
+            }
+
+        }
+        System.out.print("\nSorted array is : ");
+        for (int i = 0; i < number.length; i++) {
+            System.out.print(number[i] + " ");
+        }
+    }
+}
