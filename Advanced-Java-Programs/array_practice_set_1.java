aafe55454d1f4284c20cd65fefd539994b33f949
karanshah254 <karankshah2504@gmail.com>
2024-03-06 20:08:40 +0530

    Optimized Java code for improved performance

--- Advanced-Java-Programs/array_practice_set_1.java
+++ Advanced-Java-Programs/array_practice_set_1.java
@@ -16,7 +16,8 @@ public class array_practice_set_1 {
         return false;
     }
 
-    // Q-2 You are given an array prices where prices[i] is the price of a given stock
+    // Q-2 You are given an array prices where prices[i] is the price of a given
+    // stock
     // on the i th day. Return the maximum profit you can achieve from this
     // transaction. If you cannot achieve any profit, return 0.
     public static int question2(int array2[]) {
@@ -32,10 +33,41 @@ public class array_practice_set_1 {
         return profit;
     }
 
+    // Q-4 Gievn n non negetive intergers representing an elevation map where
+    // width od each bar is 1, computer how much water can it trap
+
+    public static int question3(int array3[]) {
+        int n = array3.length;
+        int trappedWater = 0;
+        // calculate left max array
+        int leftMax[] = new int[n];
+        leftMax[0] = array3[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(array3[i], leftMax[i - 1]);
+        }
+
+        // calculate right max array
+        int rightMax[] = new int[n];
+        rightMax[n - 1] = array3[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(array3[i], rightMax[i + 1]);
+        }
+
+        // loop for trapped water
+        for (int i = 0; i < n; i++) {
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            trappedWater += waterLevel - array3[i];
+        }
+
+        return trappedWater;
+    }
+
     public static void main(String[] args) {
         int array1[] = { 1, 2, 3, 1 };
         int array2[] = { 7, 1, 5, 3, 6, 4 };
+        int array3[] = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
         System.out.println("\nAnswer of Q-1 is " + question1(array1));
         System.out.println("\nMaximum profit after transaction is : " + question2(array2));
+        System.out.println("\nWater trapped is " + question3(array3));
     }
 }
