e5eaf89c19eb2b8369fc5c238bc7703a902e6b30
karanshah254 <karankshah2504@gmail.com>
2024-03-17 09:39:41 +0530

    Optimized Java code for improved performance

--- OOPS-Concept-Alpha/basic_concept.java
+++ OOPS-Concept-Alpha/basic_concept.java
@@ -5,6 +5,20 @@ public class basic_concept {
         System.out.println(p1.color);
         p1.setTip(10);
         System.out.println(p1.tip);
+
+        BankAccount acc = new BankAccount();
+        acc.username = "Karan Shah";
+        // acc.password = "karan&25"; "private keyword can't be accessed directly"
+        acc.setPasswd("karan&25"); // can change password but cannot access it directly
+    }
+}
+
+class BankAccount {
+    public String username;
+    private String password;
+
+    public void setPasswd(String pwd) {
+        password = pwd;
     }
 }
 
