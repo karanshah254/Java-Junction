6e1a8d3426c016a81ac0862edf78bfe3629a0a91
karanshah254 <karankshah2504@gmail.com>
2024-04-02 23:34:29 +0530

    Removed class file from repo

--- OOP-VGEC/Practical-1/program_2.java
+++
@@ -1,5 +0,0 @@
-public class program_2 {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Java, Learning Java Now and Programming is fun");
-    }
-}
--- Start-with-Java/basic.java
+++
@@ -1,8 +0,0 @@
-// class name and file-name is same in every java-file
-// boiler plate
-public class basic {
-    // main function 
-    public static void main(String args[]) {
-        System.out.println("boiler plate....");
-    }
-}
